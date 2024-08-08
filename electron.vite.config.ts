a813156a767011b7d3f219f6270b15aaf151eecb
aswnss <aswnss@gmail.com>
2024-08-08 09:58:51 +0530

    more configuration

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@/lib': resolve('src/main/lib'),
+        '@/shared': resolve('src/shared')
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
@@ -12,7 +18,13 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@/shared': resolve('src/shared'),
+        '@/hooks': resolve('src/renderer/src/hooks'),
+        '@/components': resolve('src/renderer/src/components'),
+        '@/assets': resolve('src/renderer/src/assets'),
+        '@/mocks': resolve('src/renderer/src/mocks'),
+        '@/store': resolve('src/renderer/src/store')
       }
     },
     plugins: [react()]
